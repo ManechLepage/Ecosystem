e7ba3e4df3cb80d7b8e541728f29c0f604e75af8
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2023-10-20 18:45:39 -0400

    Aded new tree mesh

--- Ecosystem/Assets/Scripts/EcosystemSimulation/LivingEntity/OakTree.cs
+++ Ecosystem/Assets/Scripts/EcosystemSimulation/LivingEntity/OakTree.cs
@@ -8,13 +8,13 @@ public class OakTree : Plant
     {
         base.Start();
 
-        float m = 650f;
+        float m = 350f;
         gameObject.transform.localScale *= m;
         m = gameObject.transform.localScale.x;
 
         // Ne fonctionne pas (tentative de mettre les arbres sur le sol)
         // print($"{gameObject.GetComponent<MeshRenderer>().bounds.size.y / 2f * (m / 1000f)} avec m={m}");
         // Le problème est que (probablement) l'origine des prefabs des arbres n'est pas au sol...
-        // gameObject.transform.position += new Vector3(0f, gameObject.GetComponent<MeshRenderer>().bounds.size.y / 2f, 0f);
+        gameObject.transform.localScale = new Vector3( m / 2f, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
     }
 }
