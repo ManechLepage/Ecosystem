413185cbebfb3e5c74b133db4bf649abca04661a
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2024-03-16 12:36:33 -0400

    Dropdown for pre-defined ecosystemData
    And made a system to choose between pre-defined and custom (working on custom).

--- Ecosystem/Assets/GameManager.cs
+++ Ecosystem/Assets/GameManager.cs
@@ -7,6 +7,7 @@ public enum GameState
 {
     Menu,
     Create,
+    Create2,
     EcosystemSimulation
 }
 
---
+++ Ecosystem/Assets/GenerationTypeSelector.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum GenerationType
+{
+    PreDefined,
+    Custom
+}
+
+
+public class GenerationTypeSelector : MonoBehaviour
+{
+    public GenerationType generatorType = GenerationType.PreDefined;
+    public TextMeshProUGUI textMesh;
+    [Space]
+    public GameObject preDefinedObject;
+    public GameObject customObject;
+    [Space]
+    public EcosystemData customData;
+
+    private EcosystemData currentPreDefinedData;
+
+    void Start()
+    {
+        currentPreDefinedData = GameManager.instance.ecosystemData;
+    }
+    
+    public void ChangedType()
+    {
+        if (generatorType == GenerationType.PreDefined)
+            generatorType = GenerationType.Custom;
+        else
+            generatorType = GenerationType.PreDefined;
+
+        UpdateData();
+    }
+
+    void UpdateData()
+    {
+        textMesh.text = generatorType == GenerationType.PreDefined ? "Pré-défini" : "Personnalisé";
+        if (generatorType == GenerationType.Custom)
+        {
+            currentPreDefinedData = GameManager.instance.ecosystemData;
+            GameManager.instance.ecosystemData = customData;
+            preDefinedObject.SetActive(false);
+            customObject.SetActive(true);
+        }
+        else
+        {
+            GameManager.instance.ecosystemData = currentPreDefinedData;
+            preDefinedObject.SetActive(true);
+            customObject.SetActive(false);
+        }
+    }
+}
